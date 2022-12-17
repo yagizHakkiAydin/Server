3c6376eb078b91e873a21ec23ea659fc9560af81
MFurkanYldz <87310054+MFurkanYldz@users.noreply.github.com>
2022-12-17 14:22:59 +0300

    Add files via upload

---
+++ BattleOfMinds.MVC/Controllers/AddController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BattleOfMinds.MVC.Controllers
+{
+    public class AddController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Index2()
+        {
+            return View("Index1");
+        }
+    }
+}
