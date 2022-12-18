d8335abfda7923fd982e781ddf2bfb24d2fcfeb9
MFurkanYldz <87310054+MFurkanYldz@users.noreply.github.com>
2022-12-18 15:59:02 +0300

    Add files via upload

---
+++ BattleOfMinds.MVC/Controllers/QuestionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BattleOfMinds.MVC.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Edit()
+        {
+            return View("Index");
+        }
+    }
+}
