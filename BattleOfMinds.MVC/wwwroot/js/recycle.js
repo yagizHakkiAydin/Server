6d60a1e3d82fd18d3829512a89944a5ee132a7e2
ErvaAksu <ervaaksu@hotmail.com>
2023-01-07 13:54:30 +0300

    update

--- BattleOfMinds.MVC/wwwroot/js/recycle.js
+++ BattleOfMinds.MVC/wwwroot/js/recycle.js
@@ -1,11 +1,9 @@
 
-
-
 const noQuestion = () => {
         
         if(question_list.length == 0){setTimeout(function () {
                 window.alert('The removed question could not be found.');
-            }, 1000);
+            }, 100000);
                 
         }
 
@@ -16,9 +14,7 @@ const addCode = (question_list, id_list) => {
     for(let i = 0 ; i < question_list.length;i++){
     let html =
         '<div class="short-question" id = ' + id_list[i] +'>\
-      '+question_list[i].substring(0,46)+'  \
-      <i class="fa-solid fa-trash" onclick ="onClick()"></i>\
-      <i class="fa-sharp fa-solid fa-pen"></i>\
+      '+question_list[i].substring(0,46)+'\
     </div>';
     
             document.getElementById("questions").innerHTML +=html;
